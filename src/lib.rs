3379fd75ed3f35043cbaab10256740cd5955b753
Per Lindgren <per.lindgren@ltu.se>
2023-07-30 22:27:36 +0200

    memory.x

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 #![no_std]
 #![no_main]
-// leave empty
+use core::arch::global_asm;
+
+global_asm! {
+    "
+    .section .init, \"ax\"
+    .global _start
+
+    _start:
+        lw x0, 0(x0)
+        sw x1, 0(x1)
+
+    .section .some_section , \"ax\"
+    .global _some_symbol
+
+    _some_symbol:
+        lw x0, 0(x0)
+        sw x1, 0(x1)
+
+"
+}
